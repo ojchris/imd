6e61403f2b50a13025e67728fa49169677d9ff0c
Chris Okpada <cokpada37@gmail.com>
2022-08-08 16:54:52 +0100

    Update test setup, create module config setting and permissions

--- web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
+++ web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
@@ -14,12 +14,17 @@ class AddYmlRepoTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'stable';
+  protected $defaultTheme = 'stark';
 
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['drupaleasy_repositories'];
+  protected static $modules = [
+    'drupaleasy_repositories',
+    'user',
+    'node',
+    'link',
+  ];
 
   /**
    * {@inheritdoc}
@@ -27,14 +32,26 @@ class AddYmlRepoTest extends BrowserTestBase {
   protected function setUp(): void {
     parent::setUp();
     // Set up the test here.
+    $config = $this->config('drupaleasy_repositories.settings');
+    $config->set('repositories', ['yml_remote' => 'yml_remote']);
+    $config->save();
+
+    // Create and login as a Drupal admin user with permission to access
+    // the DrupalEasy Repositories Settings page. This is UID=2 because
+    // UID=1 is created by FunctionalTestSetupTrait of the SetUp function
+    // ($this->installDrupal then initUserSession) of the abstrct class.
+    // The root user can be asseessed with $this->rootuser.
+    $admin_user = $this->drupalCreateUser(['drupaleasy repositories configure']);
+    $this->drupalLogin($admin_user);
+
   }
 
   /**
    * Test callback.
    */
   public function testSomething() {
-    $admin_user = $this->drupalCreateUser(['access administration pages']);
-    $this->drupalLogin($admin_user);
+    // $admin_user = $this->drupalCreateUser(['access administration pages']);
+    // $this->drupalLogin($admin_user);
     $this->drupalGet('admin');
     $this->assertSession()->elementExists('xpath', '//h1[text() = "Administration"]');
   }
