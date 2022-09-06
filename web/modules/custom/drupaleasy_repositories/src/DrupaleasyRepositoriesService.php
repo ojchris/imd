a78a3954aff8fe5478ba5332aab4dd118ea33256
Chris Okpada <cokpada37@gmail.com>
2022-09-06 07:25:31 +0100

    update the custom service by injecting plugin manager & configFactory, add the getValidatorHelpText function

--- web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
+++ web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
@@ -2,15 +2,67 @@
 
 namespace Drupal\drupaleasy_repositories;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Component\Plugin\PluginManagerInterface;
+
 /**
  * Service description.
  */
 class DrupaleasyRepositoriesService {
 
   /**
+   * The plugin manager interface.
+   *
+   * @var \Drupal\Component\Plugin\PluginManagerInterface
+   */
+  protected PluginManagerInterface $pluginManagerDrupaleasyRepositories;
+
+  /**
+   * The configuration factory interface.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected ConfigFactoryInterface $configFactory;
+
+  /**
+   * Constructs a DrupaleasyRepositories object.
+   *
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $plugin_manager_drupaleasy_repositories
+   *   The plugin manager interface.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory interface.
+   */
+  public function __construct(PluginManagerInterface $plugin_manager_drupaleasy_repositories, ConfigFactoryInterface $config_factory) {
+    $this->pluginManagerDrupaleasyRepositories = $plugin_manager_drupaleasy_repositories;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * Method description.
    */
-  public function getValidatorHelpText() {
+  public function getValidatorHelpText(): string {
+    $repository_plugins = [];
+    $enabled_repository_plugin_ids = $this->configFactory->get('drupaleasy_repositories.settings')->get('repositories') ?? [];
+
+    foreach ($enabled_repository_plugin_ids as $enabled_repository_plugin_id) {
+      if (!empty($enabled_repository_plugin_id)) {
+        $repository_plugins[] = $this->pluginManagerDrupaleasyRepositories->createInstance($enabled_repository_plugin_id);
+      }
+    }
+
+    $help = [];
+
+    /** @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesInterface $repository_plugin */
+    foreach ($repository_plugins as $repository_plugin) {
+      $help[] = $repository_plugin->validateHelpText();
+    }
+
+    if (count($help)) {
+      return implode(' ', $help);
+    }
+
+    return '';
+
   }
 
 }
