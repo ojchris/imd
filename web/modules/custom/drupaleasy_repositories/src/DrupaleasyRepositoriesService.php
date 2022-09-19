809be57d03c16e926cd0dd671c43f7408520401f
Chris Okpada <cokpada37@gmail.com>
2022-09-19 14:25:29 +0100

    update and create nodes based on submitted urser profiles url

--- web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
+++ web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
@@ -4,11 +4,16 @@ namespace Drupal\drupaleasy_repositories;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Component\Plugin\PluginManagerInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Service description.
  */
 class DrupaleasyRepositoriesService {
+
+  use StringTranslationTrait;
 
   /**
    * The plugin manager interface.
@@ -25,16 +30,37 @@ class DrupaleasyRepositoriesService {
   protected ConfigFactoryInterface $configFactory;
 
   /**
+   * The Entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The dry-run parameter.
+   *
+   * When set to "true", no nodes are created, updated, or deleted.
+   *
+   * @var bool
+   */
+  protected bool $dryRun = FALSE;
+
+  /**
    * Constructs a DrupaleasyRepositories object.
    *
    * @param \Drupal\Component\Plugin\PluginManagerInterface $plugin_manager_drupaleasy_repositories
    *   The plugin manager interface.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The configuration factory interface.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity_type.manager service.
    */
-  public function __construct(PluginManagerInterface $plugin_manager_drupaleasy_repositories, ConfigFactoryInterface $config_factory) {
+  public function __construct(PluginManagerInterface $plugin_manager_drupaleasy_repositories,
+   ConfigFactoryInterface $config_factory,
+   EntityTypeManagerInterface $entity_type_manager) {
     $this->pluginManagerDrupaleasyRepositories = $plugin_manager_drupaleasy_repositories;
     $this->configFactory = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -63,6 +89,178 @@ class DrupaleasyRepositoriesService {
 
     return '';
 
+  }
+
+  /**
+   * Validate repository URLs.
+   *
+   * Validate the URLs are valid based on the enabled plugins and ensure they
+   * haven't been added by another user. This only validates non-yml
+   * repository URLs.
+   *
+   * @param array $urls
+   *   The urls to be validated.
+   * @param int $uid
+   *   The user id of the user submitting the URLs.
+   *
+   * @return string
+   *   Errors reported by plugins.
+   */
+  public function validateRepositoryUrls(array $urls, int $uid): string {
+    $errors = [];
+    $repository_plugins = [];
+
+    // Get IDs of enabled DrupaleasyRepository plugins.
+    $enabled_repository_plugin_ids = $this->configFactory->get('drupaleasy_repositories.settings')->get('repositories') ?? [];
+    if (!$enabled_repository_plugin_ids) {
+      return 'There are no enabled repository plugins';
+    }
+
+    // Instantiate each enabled DrupaleasyRepository plugin.
+    foreach ($enabled_repository_plugin_ids as $enabled_repository_plugin_id) {
+      if (!empty($enabled_repository_plugin_id)) {
+        $repository_plugins[] = $this->pluginManagerDrupaleasyRepositories->createInstance($enabled_repository_plugin_id);
+      }
+    }
+
+    // Loop around each Repository URL and attempt to validate.
+    foreach ($urls as $url) {
+      if (is_array($url)) {
+        if ($uri = trim($url['uri'])) {
+          $validated = FALSE;
+          // Check to see if the URI is valid for any enabled plugins.
+          /** @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesInterface $repository_plugin */
+          foreach ($repository_plugins as $repository_plugin) {
+            if ($repository_plugin->validate($uri)) {
+              $validated = TRUE;
+            }
+          }
+          if (!$validated) {
+            $errors[] = $this->t('The repository url %uri is not valid.', ['%uri' => $uri]);
+          }
+        }
+      }
+    }
+
+    if ($errors) {
+      return implode(' ', $errors);
+    }
+    // No errors found.
+    return '';
+  }
+
+  /**
+   * Update the repository nodes for a given account.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $account
+   *   The user account whose repositories to update.
+   *
+   * @return bool
+   *   TRUE if successful.
+   */
+  public function updateRepositories(EntityInterface $account): bool {
+    $repos_metadata = [];
+    // Use Null Coalesce Operator in case no repositories are enabled.
+    // See https://wiki.php.net/rfc/isset_ternary
+    $enabled_repository_plugin_ids = $this->configFactory->get('drupaleasy_repositories.settings')->get('repositories') ?? [];
+
+    foreach ($enabled_repository_plugin_ids as $enabled_repository_plugin_id) {
+      if (!empty($enabled_repository_plugin_id)) {
+        /** @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesInterface $repository_location */
+        $repository_location = $this->pluginManagerDrupaleasyRepositories->createInstance($enabled_repository_plugin_id);
+        // Loop through repository URLs.
+        foreach ($account->field_repository_url ?? [] as $url) {
+          // Check if the URL validates for this repository.
+          if ($repository_location->validate($url->uri)) {
+            // Confirm the repository exists and get metadata.
+            if ($repo_metadata = $repository_location->getRepo($url->uri)) {
+              $repos_metadata += $repo_metadata;
+            }
+          }
+        }
+      }
+    }
+    return $this->updateRepositoryNodes($repos_metadata, $account) &&
+      $this->deleteRepositoryNodes($repos_metadata, $account);
+  }
+
+  /**
+   * Update repository nodes for a given user.
+   *
+   * @param array $repos_info
+   *   Repository info from API call.
+   * @param \Drupal\Core\Entity\EntityInterface $account
+   *   The user account whose repositories to update.
+   *
+   * @return bool
+   *   TRUE if successful.
+   */
+  protected function updateRepositoryNodes(array $repos_info, EntityInterface $account): bool {
+
+    if (!$repos_info) {
+      return TRUE;
+    }
+
+    // Prepare the storage and query stuff.
+    /** @var \Drupal\Core\Entity\EntityStorageInterface $node_storage */
+    $node_storage = $this->entityTypeManager->getStorage('node');
+
+    foreach ($repos_info as $key => $info) {
+      // Calculate hash value.
+      $hash = md5(serialize($info));
+
+      // Look for repository nodes from this user with matching
+      // machine_name.
+      /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+      $query = $node_storage->getQuery();
+      $query->condition('type', 'repository')
+        ->condition('uid', $account->id())
+        ->condition('field_machine_name', $key)
+        ->condition('field_source', $info['source'])
+        ->accessCheck(FALSE);
+      $results = $query->execute();
+
+      if ($results) {
+        // If we get here, a repository node exists for this repository machine name.
+        /** @var \Drupal\node\NodeInterface $node */
+        $node = $node_storage->load(reset($results));
+
+        if ($hash != $node->get('field_hash')->value) {
+          // Something changed, update node.
+          $node->setTitle($info['label']);
+          $node->set('field_description', $info['description']);
+          $node->set('field_machine_name', $key);
+          $node->set('field_num_open_issues', $info['num_open_issues']);
+          $node->set('field_source', $info['source']);
+          $node->set('field_url', $info['url']);
+          $node->set('field_hash', $hash);
+          if (!$this->dryRun) {
+            $node->save();
+            // $this->repoUpdated($node, 'updated');
+          }
+        }
+      }
+      else {
+        // Repository node doesn't exist - create a new one.
+        /** @var \Drupal\node\NodeInterface $node */
+        $node = $node_storage->create([
+          'uid' => $account->id(),
+          'type' => 'repository',
+          'title' => $info['label'],
+          'field_description' => $info['description'],
+          'field_machine_name' => $key,
+          'field_num_open_issues' => $info['num_open_issues'],
+          'field_source' => $info['source'],
+          'field_url' => $info['url'],
+          'field_hash' => $hash,
+        ]);
+        if (!$this->dryRun) {
+          $node->save();
+          // $this->repoUpdated($node, 'created');
+        }
+      }
+    }
+    return TRUE;
   }
 
 }
--- web/modules/custom/drupaleasy_repositories/src/Form/SettingsForm.php
+++ web/modules/custom/drupaleasy_repositories/src/Form/SettingsForm.php
@@ -4,11 +4,46 @@ namespace Drupal\drupaleasy_repositories\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesPluginManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Component\Utility\Unicode;
 
 /**
  * Configure DrupalEasy Repositories settings for this site.
  */
 class SettingsForm extends ConfigFormBase {
+
+  /**
+   * The DrupalEasy repositories manager service.
+   *
+   * @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesPluginManager
+   */
+  protected DrupaleasyRepositoriesPluginManager $repositoriesManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('plugin.manager.drupaleasy_repositories')
+    );
+  }
+
+  /**
+   * Constructs an SettingsForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesPluginManager $drupaleasy_repositories_manager
+   *   The DrupalEasy repositories manager service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, DrupaleasyRepositoriesPluginManager $drupaleasy_repositories_manager) {
+    parent::__construct($config_factory);
+    $this->repositoriesManager = $drupaleasy_repositories_manager;
+  }
+
 
   /**
    * {@inheritdoc}
@@ -38,13 +73,18 @@ class SettingsForm extends ConfigFormBase {
 
     $repositories_config = $this->config('drupaleasy_repositories.settings');
 
+    $repositories = $this->repositoriesManager->getDefinitions();
+    uasort($repositories, function ($a, $b) {
+      return Unicode::strcasecmp($a['label'], $b['label']);
+    });
+    $repository_options = [];
+    foreach ($repositories as $repository => $definition) {
+      $repository_options[$repository] = $definition['label'];
+    }
+
     $form['repositories'] = [
       '#type' => 'checkboxes',
-      '#options' => [
-        'yml_remote' => $this->t('Yml remote'),
-        'github' => $this->t('Github'),
-        'bitbucket' => $this->t('Bitbucket'),
-      ],
+      '#options' => $repository_options,
       '#title' => $this->t('Repositories'),
       '#default_value' => $repositories_config->get('repositories') ?: [],
     ];
