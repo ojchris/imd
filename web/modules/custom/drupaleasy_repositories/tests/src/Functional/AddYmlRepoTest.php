4707e82a0ceb9ec771a718973ab880e3256d50ff
Chris Okpada <cokpada37@gmail.com>
2022-09-20 16:11:52 +0100

    add incomplete testRemoveYmltest function to test yml node removal

--- web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
+++ web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
@@ -166,7 +166,67 @@ class AddYmlRepoTest extends BrowserTestBase {
     $session->assert($node->field_source->value == 'yml_remote', 'Source does not match.');
     $session->assert($node->title->value == 'The Batman repository', 'Label does not match.');
     $session->assert($node->field_description->value == 'This is where Batman keeps all his crime-fighting code.', 'Description does not match.');
-    $session->assert($node->field_number_of_issues->value == '6', 'Number of issues does not match.');
+    $session->assert($node->field_number_of_open_issues->value == '6', 'Number of issues does not match.');
+
+  }
+
+  /**
+   * Test that a yml repo can be removed from profile by a user.
+   *
+   * This tests that a yml-based repo can be added to a user's profile and
+   * that a repository node is successfully created upon saving the profile.
+   *
+   * @return void
+   */
+  public function testRemoveYmlRepo(): void {
+    // Create and login as a Drupal user with permission to access
+    // content.
+    $user = $this->drupalCreateUser(['access content']);
+    $this->drupalLogin($user);
+
+    // Start the browsing session.
+    $session = $this->assertSession();
+
+    // Navigate to their edit profile page and confirm we can reach it.
+    $this->drupalGet('/user/' . $user->id() . '/edit');
+    // Try this with a 500 status code to see it fail.
+    $session->statusCodeEquals(200);
+
+    // Remove the test .yml file path and submit the form.
+    $edit = [
+      'field_repository_url[0][uri]' => '',
+    ];
+    $this->submitForm($edit, 'Save');
+    $session->statusCodeEquals(200);
+    $session->responseContains('The changes have been saved.');
+    // We can't check for the following message unless we also have the future
+    // drupaleasy_notify module enabled.
+    // $session->responseContains('The repo named <em class="placeholder">The
+    // Batman repository</em> has been removed');.
+
+    // Ensure there are no any repository node.
+    /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'repository')->accessCheck(FALSE);
+    $results = $query->execute();
+    $session->assert(count($results) === 1, 'One repository node was found.');
+
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $node_storage = $entity_type_manager->getStorage('node');
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = $node_storage->load(reset($results));
+
+    // Check if node exists with the given nid.
+    if ($node) {
+      $node->delete();
+    }
+
+    // Check values.
+    $session->assert($node->field_machine_name->value != 'batman-repo', 'Machine name does not match.');
+    $session->assert($node->field_source->value != 'yml_remote', 'Source does not match.');
+    $session->assert($node->title->value != 'The Batman repository', 'Label does not match.');
+    $session->assert($node->field_description->value != 'This is where Batman keeps all his crime-fighting code.', 'Description does not match.');
+    $session->assert($node->field_number_of_open_issues->value != '6', 'Number of issues does not match.');
 
   }
 
